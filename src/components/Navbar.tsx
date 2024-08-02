5ac8196d3e49e276bd9fd239f48eca5f748302d3
04Shubham <kshubhamrnc@gmail.com>
2024-08-02 21:13:53 +0530

    complete whychooseus section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,8 +20,20 @@ const Navbar = ({ className }: { className?: string }) => {
                 </Link>
                 <MenuItem
                     setActive={setActive} active={active} item="Our Courses">
-                    <HoveredLink href="/coursesweb-dev">All Courses</HoveredLink>
+                    <div className="flex flex-col text-sm space-y-4">
+                        <HoveredLink href="/coursesweb-dev">All Courses</HoveredLink>
+                        <HoveredLink href="/coursesweb-dev">Basic Music Theory</HoveredLink>
+                        <HoveredLink href="/coursesweb-dev">Advanced Composition</HoveredLink>
+                        <HoveredLink href="/coursesweb-dev">Song Writting</HoveredLink>
+                        <HoveredLink href="/coursesweb-dev">Music Production</HoveredLink>
+                    </div>
                 </MenuItem>
+                <Link href={"/contact"}>
+                    <MenuItem
+                        setActive={setActive} active={active} item="Contact Us">
+
+                    </MenuItem>
+                </Link>
             </Menu>
         </div>
     )
