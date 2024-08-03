5bbfbe970b38dcaccf151f7cf6ee16d63363d059
04Shubham <kshubhamrnc@gmail.com>
2024-08-03 10:41:09 +0530

    start UpcomingWebinars components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinara from "@/components/UpcomingWebinara";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
@@ -9,6 +11,8 @@ export default function Home() {
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <TestimonialCards/> 
+      <UpcomingWebinara/>
     </main>
   );
 }
---
+++ src/components/UpcomingWebinara.tsx
@@ -0,0 +1,9 @@
+
+
+function UpcomingWebinara() {
+  return (
+    <div>UpcomingWebinara</div>
+  )
+}
+
+export default UpcomingWebinara
